56bc83326c4b181778c6f5cf9f4dc167e5b039cf
ELAndrews <54676884+ELAndrews@users.noreply.github.com>
2019-11-05 13:05:40 +0000

    Completed step 1 and 2

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import "src/App.css";
+import { numbers } from '../../../data';
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
 /* 
@@ -7,13 +8,18 @@ import { numbers } from '../../../data'
 */
 //Import your array data to from the provided data file
 
-const Numbers = () => {
+const Numbers = (props) => {
   // STEP 2 - add the imported data to state
+  const [numberState, setNumberState] = React.useState(numbers);
   return (
     <div>
+      {numbers.map((num, index) =>{
+        return (
+          <NumberButton />
+        )
+      })}
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
     </div>
-  );
 };
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,14 +1,20 @@
 import React from "react";
-
+import "src/App.css";
+import { specials } from '../../../data';
 //import any components needed
 
 //Import your array data to from the provided data file
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
-
+  const [specialsState, setSpecialsState] = React.useState(specials);
   return (
     <div>
+      {specials.map((char, index) =>{
+        return (
+          <SpecialButton />
+        )
+      })}
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
